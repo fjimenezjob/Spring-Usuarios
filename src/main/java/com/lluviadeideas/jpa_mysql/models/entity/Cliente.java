52d0fa9ef1ce882a7e5c7ee173e256b0844635af
franjimenez.it <franjimenez@hotelbeds.com>
2019-11-26 15:20:22 +0100

    creadas clases entity para la nueva version de la pagina adaptada para hacer facturas

--- src/main/java/com/lluviadeideas/jpa_mysql/models/entity/Cliente.java
+++ src/main/java/com/lluviadeideas/jpa_mysql/models/entity/Cliente.java
@@ -1,13 +1,18 @@
 package com.lluviadeideas.jpa_mysql.models.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -29,14 +34,14 @@ public class Cliente implements Serializable {
     private Long id;
 
     @NotEmpty
-    @Size(min = 4, max = 20) //Hay muchas anotaciones para validar lo que viene de un formularo
+    @Size(min = 4, max = 20) // Hay muchas anotaciones para validar lo que viene de un formularo
     private String nombre;
 
     @NotEmpty
     private String apellido;
 
     @NotEmpty
-    @Email //Validadores: no nulo y que sea un email
+    @Email // Validadores: no nulo y que sea un email
     private String email;
 
     @NotNull
@@ -44,8 +49,15 @@ public class Cliente implements Serializable {
     @Temporal(TemporalType.DATE)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createdAt;
-    
+
+    @OneToMany(mappedBy = "cliente",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Factura> facturas;
+
     private String foto;
+
+    public Cliente() {
+        facturas = new ArrayList<Factura>();
+    }
 
     public Long getId() {
         return id;
@@ -97,5 +109,17 @@ public class Cliente implements Serializable {
 
     public void setFoto(String foto) {
         this.foto = foto;
+    }
+
+    public List<Factura> getFacturas() {
+        return facturas;
+    }
+
+    public void setFacturas(List<Factura> facturas) {
+        this.facturas = facturas;
+    }
+
+    public void addFactura(Factura factura) {
+        facturas.add(factura);
     }
 }
---
+++ src/main/java/com/lluviadeideas/jpa_mysql/models/entity/ItemFactura.java
@@ -0,0 +1,54 @@
+package com.lluviadeideas.jpa_mysql.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "facturas_items")
+public class ItemFactura implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private Integer cantidad;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id")
+    private Producto producto;
+
+    private static final long serialVersionUID = 1L;
+
+    public Double calcularImporte() {
+        return cantidad.doubleValue() * producto.getPrecio();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+}
