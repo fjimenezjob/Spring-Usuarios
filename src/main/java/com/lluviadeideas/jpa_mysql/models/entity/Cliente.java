745e8841d79d4e0a3c5bf17976b63169f583a5a8
fjimenezJOB <fjimenezjob@gmail.com>
2019-12-19 12:45:49 +0100

    creado ver detalle de la factura

--- src/main/java/com/lluviadeideas/jpa_mysql/models/entity/Cliente.java
+++ src/main/java/com/lluviadeideas/jpa_mysql/models/entity/Cliente.java
@@ -122,4 +122,11 @@ public class Cliente implements Serializable {
     public void addFactura(Factura factura) {
         facturas.add(factura);
     }
+
+    @Override
+    public String toString() {
+        return "Cliente [apellido=" + apellido + ", nombre=" + nombre + "]";
+    }
+
+
 }
