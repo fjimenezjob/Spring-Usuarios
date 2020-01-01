2c7e2e3a32061cb83ce9190399184db10e4adf4b
fjimenezJOB <fjimenezjob@gmail.com>
2020-01-01 17:46:08 +0100

    añadida clase de spring security, usuarios con rol de administrador e usuario, todo ok

--- src/main/java/com/lluviadeideas/jpa_mysql/controllers/ClienteController.java
+++ src/main/java/com/lluviadeideas/jpa_mysql/controllers/ClienteController.java
@@ -58,7 +58,7 @@ public class ClienteController {
                 .body(recurso);
     }
 
-    @RequestMapping(value = "/listar", method = RequestMethod.GET)
+    @RequestMapping(value = {"/listar", "/"}, method = RequestMethod.GET)
     public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
         Pageable pageRequest = PageRequest.of(page, 5);
         Page<Cliente> clientes = clienteService.findAll(pageRequest);
