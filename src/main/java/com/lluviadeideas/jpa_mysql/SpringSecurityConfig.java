8e02d9c41330b4cb2abf97c0d7894fc60243f3e2
franjimenez.it <franjimenez@hotelbeds.com>
2020-01-02 12:41:50 +0100

    creados entity Use y role para peticiones a base de datos

--- src/main/java/com/lluviadeideas/jpa_mysql/SpringSecurityConfig.java
+++ src/main/java/com/lluviadeideas/jpa_mysql/SpringSecurityConfig.java
@@ -1,42 +1,35 @@
 package com.lluviadeideas.jpa_mysql;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import javax.sql.DataSource;
 
 @EnableGlobalMethodSecurity(securedEnabled = true)
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private BCryptPasswordEncoder passEncoder;
 
     @Autowired
-    public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
-        PasswordEncoder encoder = passwordEncoder();
-        UserBuilder users = User.builder().passwordEncoder(encoder::encode);
-        builder.inMemoryAuthentication().withUser(users.username("admin").password("admin").roles("ADMIN", "USER"))
-                .withUser(users.username("pedro").password("pedro123").roles("USER"));
+    private DataSource dataSource;
+
+
+    @Autowired
+    public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+        build.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passEncoder)
+        .usersByUsernameQuery("SELECT username, password, enabled from users where username=?")
+        .authoritiesByUsernameQuery("SELECT u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
-        // .antMatchers("/ver/**").hasAnyRole("USER")
-        // .antMatchers("/uploads/**").hasAnyRole("USER", "ADMIN")
-        // .antMatchers("/form/**").hasAnyRole("ADMIN")
-        // .antMatchers("/eliminar/**").hasAnyRole("ADMIN")
-        // .antMatchers("/factura/**").hasAnyRole("ADMIN")
         .anyRequest().authenticated()
         .and()
         .formLogin().loginPage("/login")
