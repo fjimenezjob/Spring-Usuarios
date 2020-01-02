a65d3a00d4eb649f18c1858ee183a481aedce286
franjimenez.it <franjimenez@hotelbeds.com>
2020-01-02 08:58:20 +0100

    cierre de sesion y muestra de nombre de usuario y de rol

--- src/main/java/com/lluviadeideas/jpa_mysql/controllers/LoginController.java
+++ src/main/java/com/lluviadeideas/jpa_mysql/controllers/LoginController.java
@@ -5,15 +5,21 @@ import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal) {
+    public String login(@RequestParam(value = "error", required = false) String error,
+        Model model, Principal principal) {
 
         if(principal != null) {
             return "redirect:/";
+        }
+
+        if(error != null){
+            model.addAttribute("error", "Ha habido un error, comprueba tu usuario y contraseña o contacta con un administrador.");
         }
         
         model.addAttribute("titulo", "Login");
