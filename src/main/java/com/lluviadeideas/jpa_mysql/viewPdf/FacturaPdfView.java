a11a6ca1ec74d414d52489a878760d5841108a49
fjimenezJOB <fjimenezjob@gmail.com>
2020-01-07 15:28:49 +0100

    Mejoras en la factura en pdf

--- src/main/java/com/lluviadeideas/jpa_mysql/viewPdf/FacturaPdfView.java
+++ src/main/java/com/lluviadeideas/jpa_mysql/viewPdf/FacturaPdfView.java
@@ -1,5 +1,6 @@
 package com.lluviadeideas.jpa_mysql.viewPdf;
 
+import java.awt.Color;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -21,44 +22,56 @@ public class FacturaPdfView extends AbstractPdfView {
 
     @Override
     protected void buildPdfDocument(Map<String, Object> model, Document document, PdfWriter writer,
-        HttpServletRequest request, HttpServletResponse response) throws Exception {
-        Factura factura =(Factura) model.get("factura");
+            HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Factura factura = (Factura) model.get("factura");
         PdfPTable tabla = new PdfPTable(1);
         tabla.setSpacingAfter(20);
-        tabla.addCell("Datos del Cliente");
+
+        PdfPCell cell = null;
+        cell = new PdfPCell(new Phrase("Datos del Cliente"));
+        cell.setBackgroundColor(new Color(184, 218, 255));
+        cell.setPadding(8f);
+        tabla.addCell(cell);
         tabla.addCell(factura.getCliente().getNombre() + " " + factura.getCliente().getApellido());
         tabla.addCell(factura.getCliente().getEmail());
 
         PdfPTable tablaDos = new PdfPTable(1);
         tablaDos.setSpacingAfter(20);
-        tablaDos.addCell("Datos de la Factura");
-        tablaDos.addCell("Folio : " +factura.getId());
+
+        cell = new PdfPCell(new Phrase("Datos de la Factura"));
+        cell.setBackgroundColor(new Color(195, 230, 203));
+        cell.setPadding(8f);
+
+        tablaDos.addCell(cell);
+        tablaDos.addCell("Folio : " + factura.getId());
         tablaDos.addCell("Descripción : " + factura.getDescripcion());
         tablaDos.addCell("Fecha : " + factura.getCreated_At());
 
         PdfPTable tablaTres = new PdfPTable(4);
+        tablaTres.setWidths(new float [] {2.5f, 1, 1, 1});
         tablaTres.addCell("Producto");
         tablaTres.addCell("Precio");
         tablaTres.addCell("Cantidad");
         tablaTres.addCell("Total");
 
-        for(ItemFactura item : factura.getItems()) {
+        for (ItemFactura item : factura.getItems()) {
             tablaTres.addCell(item.getProducto().getNombre());
             tablaTres.addCell(item.getProducto().getPrecio().toString());
-            tablaTres.addCell(item.getCantidad().toString());
+            cell = new PdfPCell(new Phrase(item.getCantidad().toString()));
+            cell.setHorizontalAlignment(PdfPCell.ALIGN_CENTER);
+            tablaTres.addCell(cell);
             tablaTres.addCell(item.calcularImporte().toString());
         }
-        
+
         document.add(tabla);
         document.add(tablaDos);
 
-        PdfPCell cell = new PdfPCell(new Phrase("Total: "));
-        cell.setColspan(3);
-        cell.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
-        tablaTres.addCell(cell);
+        PdfPCell cell1 = new PdfPCell(new Phrase("Total : "));
+        cell1.setColspan(3);
+        cell1.setHorizontalAlignment(PdfPCell.ALIGN_RIGHT);
+        tablaTres.addCell(cell1);
         tablaTres.addCell(factura.getTotal().toString());
         document.add(tablaTres);
     }
-
 
 }
